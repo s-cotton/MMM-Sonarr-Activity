69984671321c14443d0144fc32d33c7e3d4998dc
s-cotton <cot.s.ton@gmail.com>
2017-03-07 15:20:48 -0500

    Added missing moment library in getScripts

--- MMM-Sonarr-Activity.js
+++ MMM-Sonarr-Activity.js
@@ -173,6 +173,7 @@ Module.register("MMM-Sonarr-Activity", {
 
     getScripts: function() {
         return [
+            'moment.js',
             'https://code.jquery.com/jquery-2.2.3.min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
             'https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.3.3/backbone-min.js',
